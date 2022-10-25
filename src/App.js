22b73d0808f401dfb61cb9bc235c60ea0113eea2
miunmnn <esteban.principe@utec.edu.pe>
2022-10-24 23:45:56 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Header from './Header'; //Include Header
+import Footer from './Footer';
+import Home from './Home'
+import About from './About'
+import Products from './Products'
+import Contact from './Contact'
+import SingleProduct from './SingleProduct'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      
+        <BrowserRouter>
+          <Header></Header>
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/single-product" element={<SingleProduct />} />
+            </Routes>
+            <Footer></Footer>
+        </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
